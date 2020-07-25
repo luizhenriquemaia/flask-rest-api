58f6b280fbfd92e2046a7156a859a51dd7d40c33
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-07-25 11:04:38 -0300

    set shell context

--- manage.py
+++ manage.py
@@ -1,11 +1,21 @@
 from app import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
 # set migrations
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        app=app,
+        db=db,
+        User=User
+    )
 
 
 if __name__ == '__main__':
