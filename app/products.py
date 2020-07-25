4e3076f0baa5c53291a7785035615588ab9e172d
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-07-25 00:08:51 -0300

    post products

--- app/products.py
+++ app/products.py
@@ -1,21 +1,42 @@
-from flask import Blueprint
+from . import db
+from flask import Blueprint, request, jsonify, Response
 from .models import Product
 from .serializer import ProductSerializer
+import json
 
 
 bp_products = Blueprint('products', __name__)
 
 
-@bp_products.route('/show-products', methods=['GET'])
-def get():
+@bp_products.route('/api/show-products', methods=['GET'])
+def list_products():
     serializer = ProductSerializer(many=True)
     query = Product.query.all()
+    result = serializer.dump(query)
     if query != []:
-        return serializer.jsonify(query), 200
+        return jsonify(result), 200
     else:
-        return serializer.jsonify(query), 204
+        return jsonify(result), 204
 
 
-@bp_products.route('/product', methods=['GET', 'POST', 'DELETE', 'PUT'])
-def delete():
-    ...
+@bp_products.route('/api/product', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def product_api():
+    serializer = ProductSerializer(many=False)
+
+    if request.method == 'POST':
+        if request.is_json:
+            name = request.json['name']
+            description = request.json['description']
+            price = request.json['price']
+            new_product = Product(
+                name, description, price
+            )
+            db.session.add(new_product)
+            db.session.commit()
+            return Response(response={'data': new_product, 'message': "created"}, status=201)
+        else:
+            return Response(response={'data': '', 'message': "bad request"}, status=404)
+    
+    #elif request.method == 'GET':
+
+    
--- test_products.py
+++ test_products.py
@@ -1,7 +1,10 @@
-from flask import Flask
-from app import app
+from flask import Flask, jsonify
+from app import app, db
+from app.models import Product
+from app.serializer import ProductSerializer
 import os
 import pytest
+import json
 
 
 @pytest.fixture
@@ -12,5 +15,27 @@ def client():
 
 
 def test_show_products(client):
-    response = client.get('/show-products')
-    assert response.status_code == 204
+    response = client.get('/api/show-products')
+    assert response.status_code == 200
+    #assert response.status_code == 204
+
+
+def test_add_product(client):
+    serializer = ProductSerializer(many=False)
+    data = {
+        'name': 'yellow t-shirt3',
+        'description': 'yellow t-shirt2 of nike',
+        'price': 400.00
+    }
+    
+    db.session.flush()
+    response = client.post('/api/product', json=serializer.dump(data), headers={"Content-Type": "application/json"})
+    db.session.rollback()
+    assert response.status_code == 201
+
+
+# def test_add_product_without_json(client):
+#     # bad request test
+#     data = ["yellow t-shirt", "yellow t-shirt of nike", 400.00]
+#     response = client.post('/api/product', json=data, headers={"Content=Type": "application/json"})
+#     assert response.status_code == 400
